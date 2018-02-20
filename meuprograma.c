73faab0a480a5405a55574c57d51512925ce5ddd
Vinicius <vcv_varella@hotmail.com>
2018-02-20 10:09:30 -0300

    Implementação iterativa

--- meuprograma.c
+++ meuprograma.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 
 int fib(int n){
-	return 0;
+	int f, anterior, aux, f;
+	anterior = 1;
+	f = 1;
+	for(i = 2; i < n; i++){
+		aux = f;
+		f = f + anterior;
+		anterior = aux;
+	}
+	return f; 
 }
 
 void main(){

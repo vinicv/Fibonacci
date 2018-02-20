4fd0990acae0cb4455bf6ff9ab16473c87fc4e36
Vinicius <vcv_varella@hotmail.com>
2018-02-20 09:49:15 -0300

    Arquivos iniciais

---
+++ meuprograma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n){
+	return 0;
+}
+
+void main(){
+	int n;
+	
+	for(n = 1; n <= 20; n++){
+		printf("%d ", fib(n));
+	}
+	
+	print("\n");
+}
+	
+
